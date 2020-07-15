ea330bda4ab1ad579ad0382f32150f950f7073e4
shinnegishi <shinnegishi1986@gamil.com>
2020-07-15 09:57:19 +0900

    Added Try Catch and update chromedriver

--- amazon_test.py
+++ amazon_test.py
@@ -25,27 +25,29 @@ service = Service('./chromedriver.exe')
 service.start()
 #browser = webdriver.Remote(service.service_url,capabilities)
 browser = webdriver.Remote(service.service_url,desired_capabilities=chrome_options.to_capabilities())
-browser.get('https://www.amazon.co.jp/gp/bestsellers/books/466282')
+try:
+    browser.get('https://www.amazon.co.jp/gp/bestsellers/books/466282')
+    with open('amazon_%s.txt'%(timestamp), 'w') as f:
+        while True: #continue until getting the last page
+            books = browser.find_elements_by_css_selector('li.zg-item-immersion')
+            book_list = []
+            for book in books:
+                book_title = book.find_element_by_css_selector('div.p13n-sc-truncated').get_attribute("title").encode('utf-8').strip('\n')
+                if book_title in book_list:
+                    pass
+                else:
+                    if(len(book_title) > 0):
+                        book_list.append(book_title)
+            f.write("\n".join(book_list))
 
-with open('amazon_%s.txt'%(timestamp), 'w') as f:
-    while True: #continue until getting the last page
-        books = browser.find_elements_by_css_selector('li.zg-item-immersion')
-        book_list = []
-        for book in books:
-            book_title = book.find_element_by_css_selector('div.p13n-sc-truncated').get_attribute("title").encode('utf-8').strip('\n')
-            if book_title in book_list:
-                pass
+            #time.sleep(20) # Let the user actually see something!
+            #browser.quit()
+            #page+=1
+            if len(browser.find_elements_by_css_selector('li.a-last a')) > 0:
+                next_url = browser.find_element_by_css_selector('li.a-last a').get_attribute("href")
+                browser.get(next_url)
             else:
-                if(len(book_title) > 0):
-                    book_list.append(book_title)
-        f.write("\n".join(book_list))
-
-        #time.sleep(20) # Let the user actually see something!
-        #browser.quit()
-        #page+=1
-        if len(browser.find_elements_by_css_selector('li.a-last a')) > 0:
-            next_url = browser.find_element_by_css_selector('li.a-last a').get_attribute("href")
-            browser.get(next_url)
-        else:
-            time.sleep(1)
-            browser.quit()
+                time.sleep(1)
+                browser.quit()
+except Exception as e:
+    print(e.message)
